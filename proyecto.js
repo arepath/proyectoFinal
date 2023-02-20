7b1ae9fad9322e3a809c2f8fc00d9ad8f40db628
Estuarth Gonzalez <egonzalez@bytesw.com>
2023-02-20 17:07:23 -0600

    se optimiza en lineas el codigo base

--- proyecto.js
+++ proyecto.js
@@ -1,124 +1,62 @@
-//Variables utiles 
-//Precio base de la cotización, en quetzales, lo puede cambiar
-var precio_base = 2000;
+const precio_base = 2000;
+const edad_18 = 0.1; // 10%
+const edad_25 = 0.2; // 20%
+const edad_50 = 0.3; // 30%
+const hijos_recargo = 0.2; // 20%
+const salario_recargo = 0.05; // 05%
+const propiedad_recargo = 0.35; // 35%
 
-//Valores de los recargos 
-var edad_18 = 0.1; // 10%
-var edad_25 = 0.2; // 20%
-var edad_50 = 0.3; // 30%
-
-var casado_18 = 0.1; // 10%
-var casado_25 = 0.2; // 20%
-var casado_50 = 0.3; // 30%
-
-var hijos_recargo = 0.2; // 20%
-
-var salario_recargo = 0.05; // 05%
-
-var propiedad_recargo = 0.35; // 35%
-
-
-//Recargo
 var recargo = 0;
 var recargo_total = 0;
-
-//Precio final 
 var precio_final = 0;
-
-//Mensajes de alerta para ingresar datos
 
 var nombre = prompt("Ingrese su nombre, por favor");
 var edad = prompt("¿Cuantos años tiene? Ingrese solamente números ");
-if (edad <= 18) {
-  alert("Debe ser mayor de edad para adquirir un seguro");
+if (edad <= 18) { alert("Debe ser mayor de edad para adquirir un seguro");
 } else {
-  var casado = prompt("¿Está casado actualmente?", "si/no");
-  //Comprobamos la edad del cónyuge, solamente si se está casado/a
-  var edad_conyuge;
-  if ("SI" == casado.toUpperCase()) {
-    edad_conyuge = prompt("¿Que edad tiene su esposo/a?");
-  }
-  //convirtiendo las edades ingresadas a números 
-  var edad_numero = parseInt(edad);
-  var edad_conyuge_numero = 0;
-  //convirtiendo la edad del cónyuge si se esta casado/a
-  if ("SI" == casado.toUpperCase()) {
-    edad_conyuge_numero = parseInt(edad_conyuge);
-  }
-
-  var hijos = prompt("¿Tiene hijos o hijas?", "si/no")
-  //Comprobamos la cantidad de hijos solamente si los tienen
-  var cantidad_hijos;
-  var cantidad_hijos_numero = 0;
-  if ("SI" == hijos.toUpperCase()) {
-    cantidad_hijos = prompt("¿Cuantos hijos tiene?");
-  }
-  /**
-   * 1. convierta la cantidad de hijos a numero
-   */
-  if ("SI" == hijos.toUpperCase()) {
-    cantidad_hijos_numero = parseInt(cantidad_hijos);
-  }
-
   var salario = prompt("Ingrese su salario en numeros");
   recargo = salario * salario_recargo;
   recargo_total = recargo_total + recargo;
-
+  var casado = prompt("¿Está casado actualmente?", "si/no");
+  var edad_conyuge;
+  if ("SI" == casado.toUpperCase()) { edad_conyuge = prompt("¿Que edad tiene su esposo/a?"); }
+  var hijos = prompt("¿Tiene hijos o hijas?", "si/no")
+  var cantidad_hijos;
+  if ("SI" == hijos.toUpperCase()) {
+    cantidad_hijos = prompt("¿Cuantos hijos tiene?");
+    if (cantidad_hijos > 0) {
+      recargo = (precio_base * hijos_recargo) * cantidad_hijos;
+      recargo_total = recargo_total + recargo;
+    };
+  }
   var propiedades = prompt("¿Cuenta con propiedades?", "si/no");
   var cantidad_propiedades;
-  var cantidad_propiedades_numero = 0;
   if ("SI" == propiedades.toUpperCase()) {
     cantidad_propiedades = prompt("¿Cuantas propiedades tiene?");
+    if (cantidad_propiedades > 0) {
+      recargo = (precio_base * propiedad_recargo) * cantidad_propiedades;
+      recargo_total = recargo_total + recargo;
+    };
   };
 
-  if ("SI" == propiedades.toUpperCase()) {
-    cantidad_propiedades_numero = parseInt(cantidad_propiedades);
-  };
-
-  if (cantidad_propiedades_numero > 0) {
-    recargo = (precio_base * propiedad_recargo) * cantidad_propiedades_numero;
-    recargo_total = recargo_total + recargo;
-  };
-
-  //Aquí debe calcular el recargo total basado en las respuestas ingresadas
-
-  //Aquí es donde debe de calcular los recargos y el valor final
-  //Ejemplo (Debe completar los condicionales): Recargo por edad del asegurado 
-  if (edad_numero >= 18 && edad_numero <= 24) {
-    //Calculamos el recargo en base a la edad
+  if (edad >= 18 && edad <= 24) {
     recargo = precio_base * edad_18;
-    //Sumamos todos los recargos que hemos obtenido
     recargo_total = recargo_total + recargo;
-  } else if (edad_numero >= 25 && edad_numero <= 49) {
+  } else if (edad >= 25 && edad <= 49) {
     recargo = precio_base * edad_25;
     recargo_total = recargo_total + recargo;
-  } else if (edad_numero >= 50) {
+  } else if (edad >= 50) {
     recargo = precio_base * edad_50;
     recargo_total = recargo_total + recargo;
   };
-  //aqui puede colocar un else if() con el siguiente rango
-
-  /** 
-   * 2. Recargo por la edad del conyuge
-   */
-  if (edad_conyuge_numero >= 18 && edad_conyuge_numero <= 24) {
-    //Calculamos el recargo en base a la edad
+  if (edad_conyuge >= 18 && edad_conyuge <= 24) {
     recargo = precio_base * edad_18;
-    //Sumamos todos los recargos que hemos obtenido
     recargo_total = recargo_total + recargo;
-  } else if (edad_conyuge_numero >= 25 && edad_conyuge_numero <= 49) {
+  } else if (edad_conyuge >= 25 && edad_conyuge <= 49) {
     recargo = precio_base * edad_25;
     recargo_total = recargo_total + recargo;
-  } else if (edad_conyuge_numero >= 50) {
+  } else if (edad_conyuge >= 50) {
     recargo = precio_base * edad_50;
-    recargo_total = recargo_total + recargo;
-  };
-
-  /**
-   * 3. Recargo por la cantidad de hijos 
-   */
-  if (cantidad_hijos_numero > 0) {
-    recargo = (precio_base * hijos_recargo) * cantidad_hijos_numero;
     recargo_total = recargo_total + recargo;
   };
 
@@ -129,5 +67,5 @@ if (edad <= 18) {
   alert("El precio sera de: " + precio_final);
 
 };
-validadorBucle = prompt("¿Desea realizar otra cotizacion? Escriba salir para finalizar");
+//validadorBucle = prompt("¿Desea realizar otra cotizacion? Escriba salir para finalizar");
 
